12a055e6c0db95ab736db0ce8643107bfc98a017
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-05-09 10:32:00 -0600

    cambios

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+
+  file: File | any = null;
+  url_imagen: string | any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  GuardarImagen(event: any) {
+    this.file = event.target.files[0];
+    this.MostrarImagen(this.file);
+  }
+
+  MostrarImagen(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      this.url_imagen = e.target.result;
+    };
+    reader.readAsDataURL(file);
+  }
 }
